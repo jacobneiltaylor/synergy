c69701a9fef962a1436d1f066981fe82419b95eb
Jacob Neil Taylor <jake@taylornetworks.com.au>
2016-05-19 03:36:58 +1000

    Updated POSIX Test Application to use SDL2

--- test/posix.cpp
+++ test/posix.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	loader* loader = loader::getInstance();
 
-	library* sdl2lib = loader->load("libSDL.so");
+	library* sdl2lib = loader->load("libSDL2-2.0.so.0");
 
 	std::string symbol = "SDL_ShowSimpleMessageBox";
 	int result = sdl2lib->call<int>(symbol, 64, "LibSynergy", "This message box was opened through LibSynergy.",  NULL);
