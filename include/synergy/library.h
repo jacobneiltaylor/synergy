4f81a6715262138211c4986dce790ca0a8030fb9
Jacob Neil Taylor <jake@taylornetworks.com.au>
2016-05-20 00:10:09 +1000

    Added support for symbol pointer caching

--- include/synergy/library.h
+++ include/synergy/library.h
@@ -16,6 +16,7 @@ namespace taylornet {
 		private:
 			std::string name;
 			std::string filename;
+			std::map<std::string, void*> call_cache;
 
 			void* lib_ptr;
 
@@ -24,21 +25,24 @@ namespace taylornet {
 			void nativeUnload();
 			void* getNativeSymbol(const char* symbol);
 
+			void* getSymbol(std::string symbol);
+			void registerSymbol(std::string symbol, void* symbol_ptr);
+
 		public:
 			std::string getName();
 			std::string getFilename();
 			
-			template <typename T> T call(std::string symbol, T returntype)
+			template <typename T> T call(std::string symbol)
 			{
 				typedef T(*symbolcall)();
-				symbolcall function_ptr = (symbolcall) this->getNativeSymbol(symbol.c_str());
+				symbolcall function_ptr = (symbolcall) this->getSymbol(symbol);
 				return function_ptr();
 			}
 
 			template <typename T, typename ...Args> T call(std::string symbol, Args... symbolargs)
 			{
 				typedef T(*symbolcall)(Args...);
-				symbolcall function_ptr = (symbolcall) this->getNativeSymbol(symbol.c_str());
+				symbolcall function_ptr = (symbolcall) this->getSymbol(symbol);
 				return function_ptr(symbolargs...);
 			}
 		};
