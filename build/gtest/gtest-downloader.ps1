e77e5bb1f5159a482fb015e0a2dd7784c12a181b
Jacob Neil Taylor <jake@taylornetworks.com.au>
2016-05-22 15:20:57 +1000

    Added script to automate Googletest aquisition

---
+++ build/gtest/gtest-downloader.ps1
@@ -0,0 +1,15 @@
+﻿# Download Google Test
+$webclient = New-Object System.Net.WebClient
+$url = "https://github.com/google/googletest/archive/release-1.7.0.zip"
+$dlfilename = $env:TEMP+"\release-1.7.0.zip"
+$webclient.DownloadFile($url, $dlfilename)
+
+# Unzip to test directory
+$shellapp = New-Object -ComObject shell.application
+$zipfile = $shellapp.namespace($dlfilename)
+$destination = $shellapp.namespace($PSScriptRoot);
+$files = $zipfile.items()
+$destination.Copyhere($files)
+
+# Rename directory
+Rename-Item $PSScriptRoot"\googletest-release-1.7.0" "googletest"
